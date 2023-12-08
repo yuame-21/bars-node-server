8387457f3395e681e35cbc3d7839c1c8ba8e203c
nagiaboushaca <nagishaca@gmail.com>
2023-12-08 14:38:58 -0500

    meow

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from 'mongoose';
+mongoose.connect("mongodb://127.0.0.1:27017/barz");
 const app = express();
 app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
