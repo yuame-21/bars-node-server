8c3c5dc903db611cd2352092879eb83b1f5f56ed
yuame-21 <yu.ame@northeastern.edu>
2023-12-12 11:18:53 -0500

    adding username to posts

--- posts/dao.js
+++ posts/dao.js
@@ -4,6 +4,8 @@ export const findAllPosts = () => model.find();
 export const findPostById = (postId) => model.findById(postId);
 export const findPostsByUser = (userId) => 
     model.find({ userId: userId });
+export const findPostsByUsername = (username) => 
+    model.find({username: username}); 
 export const updatePost = (postId, post) =>
     model.updateOne({ _id: postId }, { $set: post });
 export const deletePost = (postId) => model.deleteOne({ _id: postId });
